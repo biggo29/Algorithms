7bd392eb31aa44042e97562354ef3ab612b893b2
Shoaib Shahriar <shoaibshahriar29@gmail.com>
2020-03-04 15:09:38 +0600

    update

--- BasicProblems/Program.cs
+++ BasicProblems/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
 namespace BasicProblems
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //print reverse string
+            //declaring string for reverse. 
+            string sentense = "I eat";
+            string reverseString = "";
+            //printing reverse string
+            Console.WriteLine("The input string is: {0}",sentense);
+            //calling ReverseString class
+            Console.WriteLine("The reverse string of the input is: {0}", ReverseString.StringReverse(sentense));
+            //print "I eat" to "I tae"
+            //--------------------------
+            //declate the seperator
+            char[] seperator = { ' ' };
+            //split the sentence with space
+            String[] stringList = sentense.Split(seperator);
+            //calling reverse method and concatenating with 1st string 
+            reverseString = stringList[0] +' '+ ReverseString.StringReverse(stringList[1]);
+            //printing the reverse string
+            Console.WriteLine("The reverse string of the input is: {0}", reverseString);
         }
     }
 }
---
+++ BasicProblems/ReverseString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProblems
+{
+    public class ReverseString
+    {
+        public static string StringReverse(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
